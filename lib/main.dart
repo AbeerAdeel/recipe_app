8a83787d9eeb6d696f5ecdfd23ff60356561efde
AbeerAdeel <aadeel@uoguelph.ca>
2020-07-24 02:46:39 -0400

    Added pagination for recommended top recipes page

--- lib/main.dart
+++ lib/main.dart
@@ -33,3 +33,134 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // String _searchQuery = 'flutter';
+  // int nRepositories = 10;
+
+  // void changeQuery(String query) {
+  //   setState(() {
+  //     print(query);
+  //     _searchQuery = query ?? 'flutter';
+  //   });
+  // }
+  int skip = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            // TextField(
+            //   decoration: const InputDecoration(
+            //     labelText: 'Search Query',
+            //   ),
+            //   keyboardType: TextInputType.text,
+            //   onSubmitted: changeQuery,
+            // ),
+            Query(
+              options: QueryOptions(
+                documentNode: gql(getTopRecipesQuery),
+                variables: {
+                  'email': 'adeelabeer@gmail.com',
+                  'skip': skip,
+                  'limit': 1,
+                },
+                //pollInterval: 10,
+              ),
+              builder: (QueryResult result, {refetch, FetchMore fetchMore}) {
+                if (result.hasException) {
+                  return Text(result.exception.toString());
+                }
+
+                if (result.loading && result.data == null) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (result.data == null && !result.hasException) {
+                  return const Text(
+                      'Both data and errors are null, this is a known bug after refactoring, you might have forgotten to set Github token');
+                }
+
+                // result.data can be either a [List<dynamic>] or a [Map<String, dynamic>]
+                print(result.data);
+                final repositories =
+                    (result.data['getTopRecipes'] as List<dynamic>);
+                skip = skip + 1;
+                final opts = FetchMoreOptions(
+                  variables: {'skip': skip},
+                  updateQuery: (previousResultData, fetchMoreResultData) {
+                    final repos = [
+                      ...previousResultData['getTopRecipes'],
+                      ...fetchMoreResultData['getTopRecipes']
+                    ];
+
+                    print(repos);
+                    fetchMoreResultData['getTopRecipes'] = repos;
+                    return fetchMoreResultData;
+                  },
+                );
+                // print(repositories);
+                return Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      for (var repository in repositories)
+                        ListTile(
+                          // leading: (repository['viewerHasStarred'] as bool)
+                          //     ? const Icon(
+                          //         Icons.star,
+                          //         color: Colors.amber,
+                          //       )
+                          //     : const Icon(Icons.star_border),
+                          title: Text(repository['name'] as String),
+                        ),
+                      if (result.loading)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularProgressIndicator(),
+                          ],
+                        ),
+                      RaisedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Load More'),
+                          ],
+                        ),
+                        onPressed: () {
+                          fetchMore(opts);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/top_recipes.dart
+++ lib/top_recipes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:recipe_app/api.dart';
+import 'package:loadmore/loadmore.dart';
 
 class TopRecipes extends StatefulWidget {
   final String email;
@@ -10,52 +11,119 @@ class TopRecipes extends StatefulWidget {
 }
 
 class _TopRecipesState extends State<TopRecipes> {
+  int get count => list.length;
   ShapeBorder shape;
+  List<dynamic> list = [];
+  int skip = 5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Recommended Recipes"),
       ),
-      body: Center(
-          child: Query(
-        options: QueryOptions(
-            documentNode: gql(getTopRecipesQuery),
-            variables: {'email': widget.email, 'skip': 0, 'limit': 5}),
-        builder: (QueryResult result,
-            {VoidCallback refetch, FetchMore fetchMore}) {
-          if (result.loading) {
-            return CircularProgressIndicator();
-          } else {
-            List recipes = result.data['getTopRecipes'];
-            return ListView.builder(
-                itemCount: recipes.length,
-                itemBuilder: (context, index) {
-                  final recipe = recipes[index];
-                  return SafeArea(
-                    top: false,
-                    bottom: false,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 338.0,
-                            child: Card(
-                              // This ensures that the Card's children are clipped correctly.
-                              clipBehavior: Clip.antiAlias,
-                              shape: shape,
-                              child: RecipeContent(recipe: recipe),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Query(
+              options: QueryOptions(
+                documentNode: gql(getTopRecipesQuery),
+                variables: {
+                  'email': 'adeelabeer@gmail.com',
+                  'skip': 0,
+                  'limit': 5,
+                },
+                //pollInterval: 10,
+              ),
+              builder: (QueryResult result, {refetch, FetchMore fetchMore}) {
+                if (result.hasException) {
+                  return Text(result.exception.toString());
+                }
+
+                if (result.loading && result.data == null) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (result.data == null && !result.hasException) {
+                  return const Text(
+                      'Both data and errors are null, this is a known bug after refactoring, you might have forgotten to set Github token');
+                }
+
+                print(skip);
+                final repositories =
+                    (result.data['getTopRecipes'] as List<dynamic>);
+
+                final opts = FetchMoreOptions(
+                  variables: {'skip': skip},
+                  updateQuery: (previousResultData, fetchMoreResultData) {
+                    final repos = [
+                      ...previousResultData['getTopRecipes'],
+                      ...fetchMoreResultData['getTopRecipes']
+                    ];
+                    fetchMoreResultData['getTopRecipes'] = repos;
+                    return fetchMoreResultData;
+                  },
+                );
+
+                if (result.loading) {
+                  skip = skip + 5;
+                }
+                // print(repositories);
+                return Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      for (var recipe in repositories)
+                        SafeArea(
+                          top: false,
+                          bottom: false,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 338.0,
+                                  child: Card(
+                                    // This ensures that the Card's children are clipped correctly.
+                                    clipBehavior: Clip.antiAlias,
+                                    shape: shape,
+                                    child: RecipeContent(recipe: recipe),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  );
-                });
-          }
-        },
-      )),
+                        ),
+                      if (result.loading)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularProgressIndicator(),
+                          ],
+                        ),
+                      RaisedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Load More'),
+                          ],
+                        ),
+                        onPressed: () {
+                          fetchMore(opts);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
